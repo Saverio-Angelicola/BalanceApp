0dddacb0d3779649a9f0fb28ad35d75a69b9ab75
Saverio-Angelicola <angelicola.saverio@gmail.com>
2022-02-24 10:21:56 +0100

    add password exception for size validation

---
+++ BalanceApp.API/Exceptions/PasswordSizeException.cs
@@ -0,0 +1,7 @@
+﻿namespace BalanceApp.API.Exceptions
+{
+    public class PasswordSizeException : Exception
+    {
+        public PasswordSizeException():base ("Password must be at least 8 characters") {}
+    }
+}
