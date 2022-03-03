68d63edc89ef3d5bf4b297274a5f844e8335c772
Saverio-Angelicola <angelicola.saverio@gmail.com>
2022-03-03 15:32:10 +0100

    create unit tests for UserId value object

---
+++ BalanceApp.Domain.UnitTests/ValueObjects/UserIdTests.cs
@@ -0,0 +1,29 @@
+﻿using BalanceApp.Domain.Exceptions;
+using BalanceApp.Domain.ValueObjects;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace BalanceApp.Domain.UnitTests.ValueObjects
+{
+    public class UserIdTests
+    {
+        [Fact]
+        public void UserId_WithCorrectGuid_ValueNotNull()
+        {
+            //Arrange
+            Guid expected = Guid.NewGuid();
+            //Act
+            UserId userId = new UserId(expected);
+            //Assert
+            userId.Value.Should().Be(expected);
+
+        }
+
+        [Fact]
+        public void UserId_WithEmptyGuid_ThrowsException()
+        {
+            Assert.Throws<EmptyUserIdException>(() => new UserId(Guid.Empty));
+        }
+    }
+}
