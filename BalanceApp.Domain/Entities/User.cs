f53a9382bb62d1e1eda83a12ff32e21e262904e9
Saverio-Angelicola <angelicola.saverio@gmail.com>
2022-03-10 14:42:02 +0100

    add profiles and remove balances

--- BalanceApp.Domain/Entities/User.cs
+++ BalanceApp.Domain/Entities/User.cs
@@ -1,5 +1,4 @@
-﻿using BalanceApp.Domain.Enums;
-using BalanceApp.Domain.Exceptions;
+﻿using BalanceApp.Domain.Exceptions;
 using BalanceApp.Domain.ValueObjects;
 
 namespace BalanceApp.Domain.Entities
@@ -7,38 +6,36 @@ namespace BalanceApp.Domain.Entities
     public class User
     {
         public UserId Id { get; private set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
         public string Email { get; private set; }
         public string UserPassword { get; private set; }
-        public readonly List<BodyData> BodyDatas;
-        public readonly List<Balance> Balances;
+
+        public readonly List<Profile> Profiles;
         public string Role { get; private set; }
         public DateTime RegisterDate { get; private set; }
 
         public User()
         {
             Id = Guid.NewGuid();
-            FirstName = string.Empty;
-            LastName = string.Empty;
             Email = string.Empty;
+            Firstname = string.Empty;
+            Lastname = string.Empty;
             UserPassword = string.Empty;
-            BodyDatas = new();
-            Balances = new();
+            Profiles = new();
             Role = "User";
             RegisterDate = DateTime.UtcNow;
         }
-        public User(Guid id, string firstName, string lastName, string email, string password)
+        public User(Guid id,string firstname, string lastname, string email, string password, string role = "User")
         {
             Id = id;
-            FirstName = firstName;
-            LastName = lastName;
             Email = email;
             UserPassword = password;
-            BodyDatas = new List<BodyData>();
-            Balances = new List<Balance>();
+            Firstname = firstname;
+            Lastname = lastname;
+            Profiles = new();
             RegisterDate = DateTime.UtcNow;
-            Role = "User";
+            Role = role;
 
         }
 
@@ -52,77 +49,15 @@ namespace BalanceApp.Domain.Entities
             UserPassword = password;
         }
 
-        public void UpdateRole(UserRole role)
+        public void UpdateRole(char role)
         {
-            switch (role)
+            Role = role switch
             {
-                case UserRole.Admin:
-                    Role = "Admin";
-                    break;
-
-                case UserRole.Doctor:
-                    Role = "Doctor";
-                    break;
-                case UserRole.User:
-                    break;
-                default:
-                    Role = "User";
-                    break;
-            }
-        }
-
-        public void AddBodyData(BodyData bodyData)
-        {
-            BodyDatas.Add(bodyData);
-        }
-
-        public void AddBodyDatas(List<BodyData> bodyDatas)
-        {
-            foreach (BodyData bodyData in bodyDatas)
-            {
-                AddBodyData(bodyData);
-            }
-        }
-
-        public void AddBalance(Balance balance)
-        {
-            bool alreadyExists = Balances.Any(balance => balance.Name == balance.Name);
-            if (alreadyExists)
-            {
-                throw new BalanceNameAlreadyExistsException(balance.Name);
-            }
-
-            Balances.Add(balance);
-        }
-
-        public void AddBalances(List<Balance> balances)
-        {
-            foreach (Balance balance in balances)
-            {
-                AddBalance(balance);
-            }
-        }
-
-        public void DeleteBalance(string balanceName)
-        {
-            Balance? balance = Balances.FirstOrDefault(balance => balance.Name == balanceName);
-            if (balance is null)
-            {
-                throw new BalanceNotFoundException(balanceName);
-            }
-
-            Balances.Remove(balance);
-        }
-
-        public Balance GetBalance(string balanceName)
-        {
-            Balance? balance = Balances.FirstOrDefault(balance => balance.Name == balanceName);
-            if (balance is null)
-            {
-                throw new BalanceNotFoundException(balanceName);
-            }
-
-            return balance;
+                'A' => "Admin",
+                'D' => "Doctor",
+                'U' => "User",
+                _ => "User",
+            };
         }
     }
 }
