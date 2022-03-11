2cd3d95a53828f0a69925cb982decc40a9c877b9
Saverio-Angelicola <angelicola.saverio@gmail.com>
2022-03-11 15:31:17 +0100

    add datas verification

--- BalanceApp.Domain/ValueObjects/Profile.cs
+++ BalanceApp.Domain/ValueObjects/Profile.cs
@@ -4,26 +4,37 @@ namespace BalanceApp.Domain.ValueObjects
 {
     public record Profile
     {
+        public Guid Id { get; private set; }
+        public double Height { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public string Gender { get; set; }
+
         public readonly List<BodyData> BodyDatas;
         public BirthDate BirthDate { get; private set; }
 
-        public Profile(string firstname, string lastname, string gender, string birthdate)
+        public Profile(Guid id, string firstname, string lastname, string gender, string birthdate, double height)
         {
-            Firstname = firstname;
-            Lastname = lastname;
-            BodyDatas = new();
-            BirthDate = BirthDate.Create(birthdate);
             if (gender != "M" || gender != "F")
             {
                 throw new BadGenderException();
             }
+
+            if (height <= 0)
+            {
+                throw new IncorrectHeightValueException();
+            }
+
+            Id = id;
+            Firstname = firstname;
+            Lastname = lastname;
+            BodyDatas = new();
+            Height = height;
+            BirthDate = BirthDate.Create(birthdate);
             Gender = gender;
         }
 
-        public Profile() 
+        public Profile()
         {
             Firstname = string.Empty;
             Lastname = string.Empty;
@@ -31,19 +42,6 @@ namespace BalanceApp.Domain.ValueObjects
             Gender = string.Empty;
             BirthDate = BirthDate.Create("01/01/2000");
 
-        }
-
-        public void AddBodyData(BodyData bodyData)
-        {
-            BodyDatas.Add(bodyData);
-        }
-
-        public void AddBodyDatas(List<BodyData> bodyDatas)
-        {
-            foreach (BodyData bodyData in bodyDatas)
-            {
-                AddBodyData(bodyData);
-            }
         }
     }
 }
