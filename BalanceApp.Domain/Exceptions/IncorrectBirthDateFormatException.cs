dc9d21bfaaa490b7837a637f7a1a021ed578049d
Saverio-Angelicola <angelicola.saverio@gmail.com>
2022-03-11 15:29:38 +0100

    add new custom exception

---
+++ BalanceApp.Domain/Exceptions/IncorrectBirthDateFormatException.cs
@@ -0,0 +1,7 @@
+﻿namespace BalanceApp.Domain.Exceptions
+{
+    public class IncorrectBirthDateFormatException : Exception
+    {
+        public IncorrectBirthDateFormatException() : base("The birth of date format is incorrect. The correct format is jj/mm/yyyy") { }
+    }
+}
---
+++ BalanceApp.Domain/Exceptions/IncorrectHeightValueException.cs
@@ -0,0 +1,7 @@
+﻿namespace BalanceApp.Domain.Exceptions
+{
+    public class IncorrectHeightValueException : Exception
+    {
+        public IncorrectHeightValueException() : base("Height value must be greater than 0") { }
+    }
+}
