8a1e832e4e388498a5f383b27a2a9ce64b7a4a2a
Saverio-Angelicola <angelicola.saverio@gmail.com>
2022-03-11 15:27:43 +0100

    add profile dtos

---
+++ BalanceApp.Application/Dtos/Profiles/UpdateProfileDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalanceApp.Application.Dtos.Profiles
+{
+    public record UpdateProfileDto
+    {
+        public double Height { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Gender { get; set; }
+
+        public UpdateProfileDto()
+        {
+            Height = double.NaN;
+            Firstname = string.Empty;
+            Lastname = string.Empty;
+            Gender = string.Empty;
+        }
+
+        public UpdateProfileDto(double height, string firstname, string lastname, string gender)
+        {
+            Height = height;
+            Firstname = firstname;
+            Lastname = lastname;
+            Gender = gender;
+        }
+    }
+}
