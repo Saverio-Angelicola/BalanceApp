8b32fb7cff4feb9d3d4bdbdfe7875bcd10222829
Saverio-Angelicola <angelicola.saverio@gmail.com>
2022-03-15 10:22:08 +0100

    refactor and splitting the code into several classes

---
+++ BalanceApp.Application/Services/interfaces/Users/IUserRegistrationService.cs
@@ -0,0 +1,15 @@
+﻿using BalanceApp.Application.Dtos.Users;
+using BalanceApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalanceApp.Application.Services.interfaces.Users
+{
+    public interface IUserRegistrationService
+    {
+        Task<User> RegisterUser(CreateUserDto createdUser);
+    }
+}
--- BalanceApp.Application/Services/interfaces/Users/IUserUpdaterService.cs
+++ BalanceApp.Application/Services/interfaces/Users/IUserUpdaterService.cs
@@ -3,15 +3,10 @@ using BalanceApp.Domain.Entities;
 
 namespace BalanceApp.Application.Services.interfaces.Users
 {
-    public interface IUserService
+    public interface IUserUpdaterService
     {
-        Task<User> GetUserByEmail(string email);
-        Task<List<User>> GetAllUser();
-        Task<User> GetUserById(Guid id);
-        Task<User> CreateUser(CreateUserDto createdUser);
         Task<User> UpdateUser(string username, UpdateUserDto updatedUser);
         Task<User> UpdatePassword(string username, UpdateUserPasswordDto passwordDto);
-        Task<User> DeleteUser(string username);
 
     }
 }
