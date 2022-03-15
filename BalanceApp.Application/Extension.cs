795d26d89cd243d90acffee7da981a03b6583ce9
Saverio-Angelicola <angelicola.saverio@gmail.com>
2022-03-15 10:26:08 +0100

    add new services (profiles, bodydata, user) into dependency injection container

--- BalanceApp.Application/Extension.cs
+++ BalanceApp.Application/Extension.cs
@@ -1,6 +1,8 @@
 ﻿using BalanceApp.Application.Services.implementations.Auth;
+using BalanceApp.Application.Services.implementations.Profiles;
 using BalanceApp.Application.Services.implementations.Users;
 using BalanceApp.Application.Services.interfaces.Auth;
+using BalanceApp.Application.Services.interfaces.Profiles;
 using BalanceApp.Application.Services.interfaces.Users;
 using BalanceApp.Domain.Entities;
 using Microsoft.AspNetCore.Identity;
@@ -13,7 +15,14 @@ namespace BalanceApp.Application
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
-            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IUserUpdaterService, UserUpdaterService>();
+            services.AddScoped<IUserRegistrationService, UserRegistrationService>();
+            services.AddScoped<IUserFetcherService, UserFetcherService>();
+            services.AddScoped<IUserDeletionService, UserDeletionService>();
+            services.AddScoped<IProfileUpdaterService, ProfileUpdaterService>();
+            services.AddScoped<IProfileFetcherService, ProfileFetcherService>();
+            services.AddScoped<IProfileCreatorService, ProfileCreatorService>();
+            services.AddScoped<IProfileDeletionService, ProfileDeletionService>();
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IAuthService, AuthService>();
 
