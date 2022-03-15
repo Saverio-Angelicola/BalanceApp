43a9c4e2c7d93130ec27826029fb947608d83b7f
Saverio-Angelicola <angelicola.saverio@gmail.com>
2022-03-15 10:24:10 +0100

    refactor and splitting code into several classes

---
+++ BalanceApp.Application/Services/implementations/Users/UserDeletionService.cs
@@ -0,0 +1,28 @@
+﻿using BalanceApp.Application.Repositories;
+using BalanceApp.Application.Services.interfaces.Users;
+using BalanceApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalanceApp.Application.Services.implementations.Users
+{
+    public class UserDeletionService : IUserDeletionService
+    {
+        private readonly IUserRepository userRepository;
+
+        public UserDeletionService(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public async Task<User> DeleteUser(string email)
+        {
+            User user = await userRepository.FindByEmail(email);
+            await userRepository.Delete(user);
+            return user;
+        }
+    }
+}
--- BalanceApp.Application/Services/implementations/Users/UserUpdaterService.cs
+++ BalanceApp.Application/Services/implementations/Users/UserUpdaterService.cs
@@ -1,5 +1,4 @@
-﻿using BalanceApp.Application.Datas;
-using BalanceApp.Application.Dtos.Users;
+﻿using BalanceApp.Application.Dtos.Users;
 using BalanceApp.Application.Exceptions;
 using BalanceApp.Application.Repositories;
 using BalanceApp.Application.Services.interfaces.Users;
@@ -8,46 +7,15 @@ using Microsoft.AspNetCore.Identity;
 
 namespace BalanceApp.Application.Services.implementations.Users
 {
-    public class UserService : IUserService
+    public class UserUpdaterService : IUserUpdaterService
     {
         private readonly IUserRepository userRepository;
         private readonly IPasswordHasher<User> passwordHasher;
 
-        public UserService(IUserRepository userRepository, IPasswordHasher<User> passwordHasher)
+        public UserUpdaterService(IUserRepository userRepository, IPasswordHasher<User> passwordHasher)
         {
             this.userRepository = userRepository;
             this.passwordHasher = passwordHasher;
-        }
-
-        public async Task<User> CreateUser(CreateUserDto createdUser)
-        {
-            User user = new(Guid.NewGuid(), createdUser.FirstName, createdUser.LastName, createdUser.Email, createdUser.Password);
-            user.UpdatePassword(passwordHasher.HashPassword(user, createdUser.Password));
-            await userRepository.Create(user);
-            return user;
-        }
-
-        public async Task<User> DeleteUser(string email)
-        {
-            User user = await userRepository.FindByEmail(email);
-            await userRepository.Delete(user);
-            return user;
-
-        }
-
-        public async Task<List<User>> GetAllUser()
-        {
-            return await userRepository.FindAll();
-        }
-
-        public async Task<User> GetUserById(Guid id)
-        {
-            return await userRepository.FindById(id);
-        }
-
-        public async Task<User> GetUserByEmail(string email)
-        {
-            return await userRepository.FindByEmail(email);
         }
 
         public async Task<User> UpdatePassword(string email, UpdateUserPasswordDto passwordDto)
