4fa915a460480b69accc0ebc3dc9aa60c95cfad5
Saverio-Angelicola <angelicola.saverio@gmail.com>
2022-03-15 10:24:33 +0100

    add services for profiles with crud actions

---
+++ BalanceApp.Application/Services/interfaces/Profiles/IProfileFetcherService.cs
@@ -0,0 +1,15 @@
+﻿using BalanceApp.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalanceApp.Application.Services.interfaces.Profiles
+{
+    public interface IProfileFetcherService
+    {
+        Task<Profile> GetProfile(string userEmail, Guid ProfileId);
+        Task<List<Profile>> GetAllProfiles(string userEmail);
+    }
+}
---
+++ BalanceApp.Application/Services/interfaces/Profiles/IProfileUpdaterService.cs
@@ -0,0 +1,15 @@
+﻿using BalanceApp.Application.Dtos.Profiles;
+using BalanceApp.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalanceApp.Application.Services.interfaces.Profiles
+{
+    public interface IProfileUpdaterService
+    {
+        void UpdateProfile(string userEmail, Guid ProfileId, UpdateProfileDto updateProfileDto);
+    }
+}
