c0ae4ffaf9e0dce5c8b9168aa6a0cc57c06ed6de
Saverio-Angelicola <angelicola.saverio@gmail.com>
2022-03-18 11:01:57 +0100

    chnage string to int for gender propertie

--- BalanceApp.Application/Dtos/Profiles/CreateProfileDto.cs
+++ BalanceApp.Application/Dtos/Profiles/CreateProfileDto.cs
@@ -5,19 +5,10 @@
         public double Height { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
-        public string Gender { get; set; }
+        public int Gender { get; set; }
         public string BirthDate { get; private set; }
 
-        public CreateProfileDto()
-        {
-            Height = double.NaN;
-            Firstname = string.Empty;
-            Lastname = string.Empty;
-            Gender = string.Empty;
-            BirthDate = string.Empty;
-        }
-
-        public CreateProfileDto(double height, string firstname, string lastname, string gender, string birthDate)
+        public CreateProfileDto(double height, string firstname, string lastname, int gender, string birthDate)
         {
             Height = height;
             Firstname = firstname;
