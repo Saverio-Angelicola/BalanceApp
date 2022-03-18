f5a09e6898214d0ada370544699a96bda2018783
Saverio-Angelicola <angelicola.saverio@gmail.com>
2022-03-18 11:02:20 +0100

    change string to int from gender propertie

--- BalanceApp.Application/Dtos/Profiles/UpdateProfileDto.cs
+++ BalanceApp.Application/Dtos/Profiles/UpdateProfileDto.cs
@@ -1,27 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BalanceApp.Application.Dtos.Profiles
+﻿namespace BalanceApp.Application.Dtos.Profiles
 {
     public record UpdateProfileDto
     {
         public double Height { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
-        public string Gender { get; set; }
+        public int Gender { get; set; }
 
-        public UpdateProfileDto()
-        {
-            Height = double.NaN;
-            Firstname = string.Empty;
-            Lastname = string.Empty;
-            Gender = string.Empty;
-        }
-
-        public UpdateProfileDto(double height, string firstname, string lastname, string gender)
+        public UpdateProfileDto(double height, string firstname, string lastname, int gender)
         {
             Height = height;
             Firstname = firstname;
