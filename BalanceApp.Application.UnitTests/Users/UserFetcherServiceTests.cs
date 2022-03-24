bb86d3bb3fcae44b0219e2167f19aa4e8ae0ce8d
Saverio-Angelicola <angelicola.saverio@gmail.com>
2022-03-24 11:21:56 +0100

    init unit test

---
+++ BalanceApp.Application.UnitTests/Users/UserFetcherServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using BalanceApp.Application.Repositories;
+using BalanceApp.Application.Services.implementations.Users;
+using BalanceApp.Domain.Entities;
+using Moq;
+using System;
+
+namespace BalanceApp.Application.UnitTests.Users
+{
+    public class UserFetcherServiceTests
+    {
+        private readonly Mock<IUserRepository> repositoryStub;
+        private readonly UserFetcherService serviceStub;
+
+        public UserFetcherServiceTests()
+        {
+            repositoryStub = new Mock<IUserRepository>();
+            serviceStub = new(repositoryStub.Object);
+        }
+
+        
+
+        
+
+        internal static User CreateRandomUser()
+        {
+            Guid guid = Guid.NewGuid();
+            return new(guid, guid.ToString(), guid.ToString(), guid.ToString(), guid.ToString());
+        }
+    }
+}
