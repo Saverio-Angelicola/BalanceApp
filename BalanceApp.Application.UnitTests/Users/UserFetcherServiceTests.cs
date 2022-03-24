a0ee597660e827cf53ca5bf0eb2714d625b21e8f
Saverio-Angelicola <angelicola.saverio@gmail.com>
2022-03-24 14:44:00 +0100

    create all unit tests

--- BalanceApp.Application.UnitTests/Users/UserFetcherServiceTests.cs
+++ BalanceApp.Application.UnitTests/Users/UserFetcherServiceTests.cs
@@ -1,25 +1,66 @@
 ﻿using BalanceApp.Application.Repositories;
 using BalanceApp.Application.Services.implementations.Users;
 using BalanceApp.Domain.Entities;
+using FluentAssertions;
 using Moq;
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace BalanceApp.Application.UnitTests.Users
 {
     public class UserFetcherServiceTests
     {
         private readonly Mock<IUserRepository> repositoryStub;
-        private readonly UserFetcherService serviceStub;
+        private readonly UserFetcherService service;
 
         public UserFetcherServiceTests()
         {
             repositoryStub = new Mock<IUserRepository>();
-            serviceStub = new(repositoryStub.Object);
+            service = new(repositoryStub.Object);
         }
 
-        
+        [Fact]
+        public async Task GetAllUser_WithUserList_ReturnsUserList()
+        {
+            //Arrange
+            List<User> expected = new List<User>()
+            {
+                CreateRandomUser(),
+                CreateRandomUser()
+            };
 
-        
+            repositoryStub.Setup(repository => repository.FindAll()).ReturnsAsync(expected);
+            //Assert
+            var result = await service.GetAllUser();
+            //Act
+            result.Should().BeEquivalentTo(expected, options=>options.ComparingByMembers<User>());
+        }
+
+        [Fact]
+        public async Task GetUserById_withUser_ReturnUser()
+        {
+            //Arrange
+            User expected = CreateRandomUser();
+            repositoryStub.Setup(repo => repo.FindById(It.IsAny<Guid>())).ReturnsAsync(expected);
+            //Act
+            var result = await service.GetUserById(It.IsAny<Guid>());
+            //Assert
+            result.Should().BeEquivalentTo(expected, options => options.ComparingByMembers<User>());
+        }
+
+        [Fact]
+        public async Task GetUserByEmail_withUser_ReturnUser()
+        {
+            //Arrange
+            User expected = CreateRandomUser();
+            repositoryStub.Setup(repo => repo.FindByEmail(It.IsAny<string>())).ReturnsAsync(expected);
+            //Act
+            var result = await service.GetUserByEmail(It.IsAny<string>());
+            //Assert
+            result.Should().BeEquivalentTo(expected, options=>options.ComparingByMembers<User>());
+        }
 
         internal static User CreateRandomUser()
         {
