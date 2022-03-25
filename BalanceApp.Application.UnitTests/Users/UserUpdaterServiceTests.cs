c33da3c8ebc94513f59f6bccd8d8e62fe5e7d0a3
Saverio-Angelicola <angelicola.saverio@gmail.com>
2022-03-25 08:23:31 +0100

    init unit test file

---
+++ BalanceApp.Application.UnitTests/Users/UserUpdaterServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using BalanceApp.Application.Repositories;
+using BalanceApp.Application.Services.implementations.Users;
+using BalanceApp.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+
+namespace BalanceApp.Application.UnitTests.Users
+{
+    public class UserUpdaterServiceTests
+    {
+        private readonly Mock<IUserRepository> repositoryStub;
+        private readonly Mock<IPasswordHasher<User>> hasherStub;
+        private readonly UserUpdaterService service;
+
+        public UserUpdaterServiceTests()
+        {
+            repositoryStub = new();
+            hasherStub = new();
+            service = new(repositoryStub.Object, hasherStub.Object);
+        }
+    }
+}
