773aacd57127e6988e81d05fd4f24fb3a30680cd
Saverio-Angelicola <angelicola.saverio@gmail.com>
2022-04-02 14:36:18 +0200

    create fetcher service for body data

---
+++ BalanceApp.Application/Services/implementations/BodyDatas/BodyDataFetcherService.cs
@@ -0,0 +1,23 @@
+﻿using BalanceApp.Application.Repositories;
+using BalanceApp.Application.Services.interfaces.BodyDatas;
+using BalanceApp.Domain.Entities;
+using BalanceApp.Domain.ValueObjects;
+
+namespace BalanceApp.Application.Services.implementations.BodyDatas
+{
+    public class BodyDataFetcherService : IBodyDataFetcherService
+    {
+        private readonly IUserRepository userRepository;
+
+        public BodyDataFetcherService(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public async Task<List<BodyData>> GetAllBodyData(string userEmail)
+        {
+            User user = await userRepository.FindByEmail(userEmail);
+            return user.BodyDataList;
+        }
+    }
+}
