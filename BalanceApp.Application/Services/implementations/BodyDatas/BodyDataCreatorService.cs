83664d9757a7f7e254d2d75a9419d171bc760d00
Saverio-Angelicola <angelicola.saverio@gmail.com>
2022-04-02 14:36:04 +0200

    add dateTime provider and remove profileId

--- BalanceApp.Application/Services/implementations/BodyDatas/BodyDataCreatorService.cs
+++ BalanceApp.Application/Services/implementations/BodyDatas/BodyDataCreatorService.cs
@@ -2,6 +2,7 @@
 using BalanceApp.Application.Exceptions;
 using BalanceApp.Application.Repositories;
 using BalanceApp.Application.Services.interfaces.BodyDatas;
+using BalanceApp.Application.Services.Providers;
 using BalanceApp.Domain.Entities;
 using BalanceApp.Domain.ValueObjects;
 
@@ -10,19 +11,21 @@ namespace BalanceApp.Application.Services.implementations.BodyDatas
     public class BodyDataCreatorService : IBodyDataCreatorService
     {
         private readonly IUserRepository userRepository;
+        private readonly IDateTimeProvider dateTimeProvider;
 
-        public BodyDataCreatorService(IUserRepository userRepository)
+        public BodyDataCreatorService(IUserRepository userRepository, IDateTimeProvider dateTimeProvider)
         {
             this.userRepository = userRepository;
+            this.dateTimeProvider = dateTimeProvider;
         }
 
-        public async Task AddBodyData(Guid profileId, string userEmail, BodyDataDto bodyDataDto)
+        public async Task AddBodyData(string userEmail, BodyDataDto bodyDataDto)
         {
             try
             {
                 User user = await userRepository.FindByEmail(userEmail);
-                BodyData bodyData = new(bodyDataDto.Weight, bodyDataDto.Height, bodyDataDto.FatMassRate, bodyDataDto.WaterRate, bodyDataDto.MuscleRate, bodyDataDto.BoneRate, bodyDataDto.HeartBeat, bodyDataDto.BodyMassIndex);
-                user.AddBodyData(profileId, bodyData);
+                BodyData bodyData = new(bodyDataDto.Weight, bodyDataDto.Height, bodyDataDto.FatMassRate, bodyDataDto.WaterRate, bodyDataDto.MuscleRate, bodyDataDto.BoneRate, bodyDataDto.HeartBeat, bodyDataDto.BodyMassIndex, dateTimeProvider.GetUtcNow());
+                user.AddBodyData(bodyData);
             }
             catch(Exception)
             {
