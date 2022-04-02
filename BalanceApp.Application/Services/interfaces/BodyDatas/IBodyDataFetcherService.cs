8fa07b58cf39eff73ca6ac8cdf3e61d2c833e4b3
Saverio-Angelicola <angelicola.saverio@gmail.com>
2022-04-02 14:37:58 +0200

    create abstraction for body data's fetcher service

---
+++ BalanceApp.Application/Services/interfaces/BodyDatas/IBodyDataFetcherService.cs
@@ -0,0 +1,14 @@
+﻿using BalanceApp.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalanceApp.Application.Services.interfaces.BodyDatas
+{
+    public interface IBodyDataFetcherService
+    {
+        Task<List<BodyData>> GetAllBodyData(string userEmail);
+    }
+}
