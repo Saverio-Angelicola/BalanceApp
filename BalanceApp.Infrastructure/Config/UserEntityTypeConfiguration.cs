ca60c2e09d6a12d70029afe4c60529c81af1f7ab
Saverio-Angelicola <angelicola.saverio@gmail.com>
2022-04-02 14:29:05 +0200

    update config for user entity

--- BalanceApp.Infrastructure/Config/UserEntityTypeConfiguration.cs
+++ BalanceApp.Infrastructure/Config/UserEntityTypeConfiguration.cs
@@ -6,42 +6,27 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BalanceApp.Infrastructure.Config
 {
-    public class UserEntityTypeConfiguration : IEntityTypeConfiguration<User>, IEntityTypeConfiguration<Profile>, IEntityTypeConfiguration<BodyData>
+    public class UserEntityTypeConfiguration : IEntityTypeConfiguration<User>, IEntityTypeConfiguration<BodyData>
     {
         public void Configure(EntityTypeBuilder<User> builder)
         {
-
+            var birthDateConverter = new ValueConverter<BirthDate, string>(b => b.ToString(),
+            b => BirthDate.Create(b));
 
             builder.HasKey(user => user.Id);
-            builder.Property(user => user.Id).HasConversion(id => id.Value, id => new UserId(id));
+            builder.Property(user => user.Id).HasConversion(id => id.Value, id => new EntityId(id));
             builder.HasIndex(user => user.Email).IsUnique();
             builder.Property(user => user.Firstname);
             builder.Property(user => user.Lastname);
             builder.Property(user => user.UserPassword);
             builder.Property(user => user.RegisterDate);
             builder.Property(user => user.Role);
-            builder.HasMany(typeof(Profile), "Profiles");
-            builder.ToTable("users");
-
-
-        }
-
-        public void Configure(EntityTypeBuilder<Profile> builder)
-        {
-            var birthDateConverter = new ValueConverter<BirthDate, string>(b => b.ToString(),
-            b => BirthDate.Create(b));
-
-            builder.Property(p => p.Id);
-            builder.Property(p => p.Firstname);
-            builder.Property(p => p.Height);
-            builder.Property(p => p.Lastname);
-            builder.Property(p => p.Gender);
             builder.Property(typeof(BirthDate), "birthdate").HasConversion(birthDateConverter);
             builder
                 .Property(p => p.BirthDate)
                 .HasConversion(b => b.ToString(), b => BirthDate.Create(b));
-            builder.HasMany(typeof(BodyData), "BodyDatas");
-            builder.ToTable("profiles");
+            builder.HasMany(typeof(BodyData), "BodyDataList");
+            builder.ToTable("users");
         }
 
         public void Configure(EntityTypeBuilder<BodyData> builder)
@@ -55,7 +40,7 @@ namespace BalanceApp.Infrastructure.Config
             builder.Property(bd => bd.HeartBeat);
             builder.Property(bd => bd.BodyMassIndex);
             builder.Property(bd => bd.CreatedAt);
-            builder.ToTable("bodyDatas");
+            builder.ToTable("bodyData");
         }
     }
 }
