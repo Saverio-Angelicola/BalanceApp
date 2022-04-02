ae29f3b9cc84f45549f2324c9fc7b1758048577c
Saverio-Angelicola <angelicola.saverio@gmail.com>
2022-04-02 14:33:53 +0200

    remove deleting dependencies

--- BalanceApp.Application/Extension.cs
+++ BalanceApp.Application/Extension.cs
@@ -1,11 +1,9 @@
 ﻿using BalanceApp.Application.Services.implementations.Auth;
 using BalanceApp.Application.Services.implementations.BodyDatas;
-using BalanceApp.Application.Services.implementations.Profiles;
 using BalanceApp.Application.Services.implementations.Security;
 using BalanceApp.Application.Services.implementations.Users;
 using BalanceApp.Application.Services.interfaces.Auth;
 using BalanceApp.Application.Services.interfaces.BodyDatas;
-using BalanceApp.Application.Services.interfaces.Profiles;
 using BalanceApp.Application.Services.interfaces.Security;
 using BalanceApp.Application.Services.interfaces.Users;
 using BalanceApp.Domain.Entities;
@@ -23,11 +21,8 @@ namespace BalanceApp.Application
             services.AddScoped<IUserRegistrationService, UserRegistrationService>();
             services.AddScoped<IUserFetcherService, UserFetcherService>();
             services.AddScoped<IUserDeletionService, UserDeletionService>();
-            services.AddScoped<IProfileUpdaterService, ProfileUpdaterService>();
-            services.AddScoped<IProfileFetcherService, ProfileFetcherService>();
-            services.AddScoped<IProfileCreatorService, ProfileCreatorService>();
-            services.AddScoped<IProfileDeletionService, ProfileDeletionService>();
             services.AddScoped<IBodyDataCreatorService, BodyDataCreatorService>();
+            services.AddScoped<IBodyDataFetcherService, BodyDataFetcherService>();
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IAuthService, AuthService>();
             services.AddScoped<IJwtHandler, JwtHandler>();
