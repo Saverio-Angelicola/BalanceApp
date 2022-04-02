10382fdca95a0e2734284b4412030f595dd403d2
Saverio-Angelicola <angelicola.saverio@gmail.com>
2022-04-02 14:32:39 +0200

    replace userId to EntityId

---
+++ BalanceApp.Domain/ValueObjects/EntityId.cs
@@ -0,0 +1,27 @@
+﻿using BalanceApp.Domain.Exceptions;
+
+namespace BalanceApp.Domain.ValueObjects
+{
+    public record EntityId
+    {
+        public Guid Value { get; }
+
+        public EntityId() { }
+
+        public EntityId(Guid value)
+        {
+            if (value == Guid.Empty)
+            {
+                throw new EmptyEntityIdException();
+            }
+
+            Value = value;
+        }
+
+        public static implicit operator Guid(EntityId id)
+            => id.Value;
+
+        public static implicit operator EntityId(Guid id)
+            => new(id);
+    }
+}
