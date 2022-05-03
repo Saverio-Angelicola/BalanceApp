5ffdc2f2430629db05cc77e661cac7595a363321
Saverio-Angelicola <angelicola.saverio@gmail.com>
2022-05-03 08:25:26 +0200

    init withings token dto

---
+++ BalanceApp.Application/Dtos/Auth/WithingsTokenDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalanceApp.Application.Dtos.Auth
+{
+    public record WithingsTokenDto(string accessToken, string refreshToken)
+    {
+        public string AccessToken { get; set; } = accessToken;
+        public string RefreshToken { get; set; } = refreshToken;
+    }
+}
