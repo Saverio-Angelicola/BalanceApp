21c2db335492d4025db1e03618056857b6858100
Saverio-Angelicola <angelicola.saverio@gmail.com>
2022-05-03 08:29:50 +0200

    remove tests for AddBodyData Method

--- BalanceApp.Domain.UnitTests/Entities/UserTests.cs
+++ BalanceApp.Domain.UnitTests/Entities/UserTests.cs
@@ -33,43 +33,6 @@ namespace BalanceApp.Domain.UnitTests.Entities
             Assert.Throws<PasswordSizeException>(() => user.UpdatePassword(expected));
         }
 
-        [Fact]
-        public void AddBodyData_WithCorrectProfile_SaveBodyData()
-        {
-            //Arrange
-            User user = CreateRandomUser();
-            BodyData expected = CreateRandomBodyData();
-            //Act
-            user.AddBodyData(expected);
-            BodyData result = user.BodyDataList.First();
-            //Assert
-            result.Should().BeEquivalentTo(expected, options => options.ComparingByMembers<BodyData>());
-        }
-
-        [Fact]
-        public void AddBodyDatas_With3Element_Save3BodyData()
-        {
-            //Arrange
-            User user = CreateRandomUser();
-            List<BodyData> expected = new()
-            {
-                CreateRandomBodyData(),
-                CreateRandomBodyData(),
-                CreateRandomBodyData()
-            };
-            //Act
-            user.AddBodyDatas(expected);
-            List<BodyData> result = user.BodyDataList;
-            //Assert
-            result.Count.Should().Be(3);
-        }
-
-        internal static BodyData CreateRandomBodyData()
-        {
-            Random random = new();
-            return new(random.NextDouble(), random.NextDouble(), random.NextDouble(), random.NextDouble(), random.NextDouble(), random.NextDouble(), random.NextDouble(), random.NextDouble(), DateTime.UtcNow);
-        }
-
         internal static User CreateRandomUser()
         {
             Guid guid = Guid.NewGuid();
