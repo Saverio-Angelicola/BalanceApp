42ba401fb754cc7b8ebb7e2615323a25d6775ad6
Saverio-Angelicola <angelicola.saverio@gmail.com>
2022-05-03 08:21:35 +0200

    create abstract dependencie for HttpClient

---
+++ BalanceApp.Infrastructure/Http/IHttpClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalanceApp.Infrastructure.Http
+{
+    public interface IHttpClient
+    {
+
+        Task<HttpResponseMessage> GetAsync(string? requestUri);
+        Task<HttpResponseMessage> GetAsync(string? requestUri, HttpCompletionOption completionOption);
+        Task<HttpResponseMessage> GetAsync(string? requestUri, HttpCompletionOption completionOption, CancellationToken cancellationToken);
+        Task<HttpResponseMessage> GetAsync(string? requestUri, CancellationToken cancellationToken);
+        Task<HttpResponseMessage> GetAsync(Uri? requestUri);
+        Task<HttpResponseMessage> GetAsync(Uri? requestUri, HttpCompletionOption completionOption);
+        Task<HttpResponseMessage> GetAsync(Uri? requestUri, HttpCompletionOption completionOption, CancellationToken cancellationToken);
+        Task<HttpResponseMessage> GetAsync(Uri? requestUri, CancellationToken cancellationToken);
+        Task<HttpResponseMessage> PatchAsync(string? requestUri, HttpContent? content);
+        Task<HttpResponseMessage> PatchAsync(string? requestUri, HttpContent? content, CancellationToken cancellationToken);
+        Task<HttpResponseMessage> PatchAsync(Uri? requestUri, HttpContent? content);
+        Task<HttpResponseMessage> PatchAsync(Uri? requestUri, HttpContent? content, CancellationToken cancellationToken);
+        Task<HttpResponseMessage> PostAsync(string? requestUri, HttpContent? content);
+        Task<HttpResponseMessage> PostAsync(string? requestUri, HttpContent? content, CancellationToken cancellationToken);
+        Task<HttpResponseMessage> PostAsync(Uri? requestUri, HttpContent? content);
+        Task<HttpResponseMessage> PostAsync(Uri? requestUri, HttpContent? content, CancellationToken cancellationToken);
+        Task<HttpResponseMessage> PutAsync(string? requestUri, HttpContent? content);
+        Task<HttpResponseMessage> PutAsync(string? requestUri, HttpContent? content, CancellationToken cancellationToken);
+        Task<HttpResponseMessage> PutAsync(Uri? requestUri, HttpContent? content);
+        Task<HttpResponseMessage> PutAsync(Uri? requestUri, HttpContent? content, CancellationToken cancellationToken);
+        Task<HttpResponseMessage> DeleteAsync(string? requestUri);
+        Task<HttpResponseMessage> DeleteAsync(string? requestUri, CancellationToken cancellationToken);
+        Task<HttpResponseMessage> DeleteAsync(Uri? requestUri);
+        Task<HttpResponseMessage> DeleteAsync(Uri? requestUri, CancellationToken cancellationToken);
+
+
+
+    }
+}
