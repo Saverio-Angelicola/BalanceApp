9de7e81aaaa3c91a0c79cc20e1802a7b8d0bb287
Saverio-Angelicola <angelicola.saverio@gmail.com>
2022-05-03 08:17:05 +0200

    add new migrations

--- BalanceApp.Infrastructure/Migrations/20220428074235_initalCreate.Designer.cs
+++ BalanceApp.Infrastructure/Migrations/20220428074235_initalCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BalanceApp.Infrastructure.Datas;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BalanceApp.Infrastructure.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20220428074235_initalCreate")]
+    partial class initalCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
