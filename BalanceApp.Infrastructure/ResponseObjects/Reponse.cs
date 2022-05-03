48bfae8fdc77d859ce47303313aa4532a5939be2
Saverio-Angelicola <angelicola.saverio@gmail.com>
2022-05-03 08:20:36 +0200

    init

---
+++ BalanceApp.Infrastructure/ResponseObjects/Reponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalanceApp.Infrastructure.ResponseObjects
+{
+    record class Reponse<T> where T : class
+    {
+        public int status { get; set; }
+        public T body { get; set; }
+    }
+}
